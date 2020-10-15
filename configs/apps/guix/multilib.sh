ef3ed4944c98cf93d6251b39f8e1995743654aae
Vadym Kochan <vadim4j@gmail.com>
2020-10-15 18:02:43 +0300

    guix: multilib: add hint how to do it in system or manifest scm
    Signed-off-by: Vadym Kochan <vadim4j@gmail.com>

--- configs/apps/guix/multilib.sh
+++ configs/apps/guix/multilib.sh
@@ -1,3 +1,9 @@
 #! /bin/sh
+# Add multilib support for Guix system
 
+# may be something should be done in manifest or system config
+# by doing the inheritance and appending the target
+# system as it is done in wine.scm:
+#
+#    https://github.com/lfam/guix/blob/master/gnu/packages/wine.scm
 guix package -i `guix build -s i686-linux gcc-toolchain`
