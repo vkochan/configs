8ca290098d656b8a225795fe3d8c3a143cbad0da
Vadim Kochan <vadim4j@gmail.com>
2020-10-23 20:39:59 +0300

    emacs: init: remove problematic settings for 26 version
    Signed-off-by: Vadim Kochan <vadim4j@gmail.com>

--- configs/apps/emacs/init.el
+++ configs/apps/emacs/init.el
@@ -583,15 +583,15 @@
 			'(("^ *\\([-]\\) "
 			   (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
-(dolist (face '((org-level-1 . 1.2)
-		(org-level-2 . 1.1)
-		(org-level-3 . 1.05)
-		(org-level-4 . 1.0)
-		(org-level-5 . 1.1)
-		(org-level-6 . 1.1)
-		(org-level-7 . 1.1)
-		(org-level-8 . 1.1)))
-  (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+; (dolist (face '((org-level-1 . 1.2)
+; 		(org-level-2 . 1.1)
+; 		(org-level-3 . 1.05)
+; 		(org-level-4 . 1.0)
+; 		(org-level-5 . 1.1)
+; 		(org-level-6 . 1.1)
+; 		(org-level-7 . 1.1)
+; 		(org-level-8 . 1.1)))
+;   (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
 
 ;; Make sure org-indent face is available
 (require 'org-indent)
