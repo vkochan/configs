fa2f49152d05ce7ff4bbf71a19f0036b4da1f47b
Vadim Kochan <vadim4j@gmail.com>
2020-11-11 14:43:48 +0200

    emacs: init: use linux style identation for C mode

--- configs/apps/emacs/init.el
+++ configs/apps/emacs/init.el
@@ -703,7 +703,7 @@
 (setq org-latex-listings-options '(("breaklines" "true")))
 (setq org-latex-listings t)
 
-(setq-default tab-always-indent 'complete)
+;;(setq-default tab-always-indent 'complete)
 
 ;; example of a function that just insert a tab char
 (defun my-insert-tab-char ()
@@ -714,3 +714,34 @@
 
 (global-set-key (kbd "TAB") 'my-insert-tab-char)
 (global-set-key (kbd "<tab>") 'my-insert-tab-char)
+
+;(require 'cc-mode)
+;(add-to-list 'c-mode-common-hook
+;	     (lambda () (setq c-syntactic-indentation nil)))
+
+(defun c-lineup-arglist-tabs-only (ignored)
+  "Line up argument lists by tabs, not spaces"
+  (let* ((anchor (c-langelem-pos c-syntactic-element))
+	 (column (c-langelem-2nd-pos c-syntactic-element))
+	 (offset (- (1+ column) anchor))
+	 (steps (floor offset c-basic-offset)))
+    (* (max steps 1)
+       c-basic-offset)))
+
+(add-hook 'c-mode-common-hook
+	  (lambda ()
+	    ;; Add kernel style
+	    (c-add-style
+	      "linux-tabs-only"
+	      '("linux" (c-offsets-alist
+			  (arglist-cont-nonempty
+			    c-lineup-gcc-asm-reg
+			    c-lineup-arglist-tabs-only))))))
+
+(add-hook 'c-mode-hook
+	  (lambda ()
+	    (let ((filename (buffer-file-name)))
+	      ;; Enable kernel mode for the appropriate files
+		(setq indent-tabs-mode t)
+		(setq show-trailing-whitespace t)
+		(c-set-style "linux-tabs-only"))))
